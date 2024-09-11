2c4addedf296667fca5b1ca1a32a900891555d9f
Hasbel <hasbelcaro@gmail.com>
2024-09-11 07:37:40 -0500

    Manejo de acciones con zustand

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   // const count = useCounterStore((state) => state.count);
   // const title = useCounterStore((state) => state.title);
 
+  // Obtener los valores del store
   const { title, count } = useCounterStore(
     (state) => ({
       count: state.count,
@@ -13,11 +14,23 @@ function App() {
     shallow
   );
 
+  // Obtener función del store
+  // const increment = useCounterStore((state) => state.increment);
+  const { increment } = useCounterStore(); //Destructuring
+
   return (
     <div>
       <h1>
         {title}: {count}
       </h1>
+
+      <button
+        onClick={() => {
+          increment(10);
+        }}
+      >
+        Increment by 10
+      </button>
     </div>
   );
 }
--- src/store/counterStore.ts
+++ src/store/counterStore.ts
@@ -3,9 +3,14 @@ import { create } from "zustand";
 interface CounterState {
   count: number;
   title: string;
+  increment: (value: number) => void;
 }
 
-export const useCounterStore = create<CounterState>(() => ({
+export const useCounterStore = create<CounterState>((set) => ({
   count: 10,
   title: "Some title",
+  increment: (value: number) =>
+    set((state) => ({
+      count: state.count + value,
+    })),
 }));
