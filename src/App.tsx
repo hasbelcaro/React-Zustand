952e583bc989c711baa0323137fed18b66ff9e17
Hasbel <hasbelcaro@gmail.com>
2024-09-11 07:18:44 -0500

    Manejo varios estados

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useCounterStore } from "./store/counterStore";
 
 function App() {
-  const count = useCounterStore((state) => state.count);
-  const title = useCounterStore((state) => state.title);
+  // const count = useCounterStore((state) => state.count);
+  // const title = useCounterStore((state) => state.title);
+
+  const values = useCounterStore((state) => ({
+    count: state.count,
+    title: state.title,
+  }));
+
+  console.log(values);
 
   return (
     <div>
-      <h1>
-        {title}: {count}
-      </h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
